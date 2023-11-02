04be56e79dd02a92e34e36039f0834b7ddc34020
Dimitsky <dimitsky@icloud.com>
2023-11-02 19:58:12 +0400

    setup gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import { useRef } from "react";
 import { addTodo } from "./features/todo/todoSlice";
 import { useAppSelector, useAppdispatch } from "./app/hooks";
 import { TodoList } from "./features/todo/TodoList";
-import './App.css';
 import { CreateList } from "./features/todo/CreateList";
 import { ClearList } from "./features/todo/ClearList";
+import type { Todo } from "./features/todo/types/types";
+import './App.css';
 
 function App() {
     const appDispatch = useAppdispatch();
     const todoList = useAppSelector((state) => state.todo);
     const appWrapperRef = useRef<HTMLDivElement | null>(null);
+
+    const sortedTodoList = [...todoList]
+        .sort((a, b) => {
+            if (a.title > b.title) return 1;
+            if (a.title < b.title) return -1;
+            return 0;
+        }).sort((a: Todo, b: Todo) => {
+            if (a.completed > b.completed) return 1;
+            if (a.completed < b.completed) return -1;
+            return 0;
+        });
 
     const handleDoubleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         const target = e.target as HTMLDivElement;
@@ -26,10 +38,10 @@ function App() {
             ref={appWrapperRef}
             onDoubleClick={handleDoubleClick}
         >
-            {todoList.length ? (
+            {sortedTodoList.length ? (
                 <>
                     <ClearList />
-                    <TodoList list={todoList}/>
+                    <TodoList list={sortedTodoList}/>
                 </>
             ) : (
                 <CreateList />
